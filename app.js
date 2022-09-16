e1dacab09b219fe84669ccd4a826f9ca039df50c
JessicaFonseca0213 <jessicafonseca2501@hotmail.com>
2022-09-15 21:05:52 -0500

    Actualizacion Jessi

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.listen(3000,() =>{
     console.log('Servidor en puerto 3000 OK');
 });
 
-app.get('/', (req,res)=>{
-res.sendFile(path.join(__dirname, '/views/registro.html'));
+app.get('/detalleproducto', (req,res)=>{
+res.sendFile(path.join(__dirname, '/views/detalleproducto.html'));
 });
+
+app.get('/registro', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/registro.html'));
+    });
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+    });
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+    });
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+    });
