600f8316b9e729f844bc47f87e477bb59fe0e67d
celulosa <amartinmesel@hotmail.com>
2022-09-20 18:41:02 -0300

    commit con view login y href

--- app.js
+++ app.js
@@ -28,3 +28,7 @@ app.get('/login', (req,res)=>{
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
     });
+    app.get('/login', (req,res)=>{
+        res.sendFile(path.join(__dirname, '/views/login.html'));
+        });
+    
