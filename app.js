fa79501331ad35ed1ea5080270a7f7bc40883ea4
celulosa <amartinmesel@hotmail.com>
2022-09-22 14:24:02 -0300

    home

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/login', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/login.html'));
     });
 
-app.get('/', (req,res)=>{
+app.get('/home', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
     });
 app.get('/registro', (req,res)=>{
