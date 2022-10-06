8aa7f823b4f4420987a3dcb0a692730c1b2b3754
celulosa <amartinmesel@hotmail.com>
2022-10-05 23:23:57 -0300

    testeo beta branch

---
+++ src/controllers/usersController.js
@@ -0,0 +1,11 @@
+const controlador ={
+    login: (req , res) => {
+        res.render('users/login');
+    },
+
+    registro: (req , res) => {
+        res.render('users/registro');
+    },
+}   
+
+module.exports = controlador;
