0643240304b7dd958323ca74c2a75276ebe3f5c3
celulosa <amartinmesel@hotmail.com>
2022-10-20 17:03:30 -0300

    multer carga

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path')
 
 const productsController = require('../controllers/productsController')
 /* diskStorage Multer*/
 const configuracionImagen = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, path.join(__dirname,'../../public/images'));
     },
     filename: function(req, file, cb) {
-        let imageName = Date.now() + file.originalname ;
+        let imageName = Date.now() + path.extname(file.originalname);
         cb(null, imageName);
     }
 });
@@ -19,12 +20,11 @@ const uploadFile = multer({ storage: configuracionImagen});
 router.get('/', productsController.home); 
 router.get('/carrito', productsController.carrito);
 router.get('/crearproducto', productsController.crear);
-router.post('/crearproducto', productsController.store); 
+router.post('/crearproducto',uploadFile.single('imageProduct'), productsController.store); 
 router.get('/detalleproducto/:id', productsController.detalle); 
 router.get('/editarproducto/:id', productsController.editar); 
-router.put('/editarproducto/:id',uploadFile.single('imageProduct'),  productsController.update);
+router.put('/editarproducto/:id',uploadFile.single('imageProductEdit'),  productsController.update);
 router.delete('/delete/:id', productsController.destroy); 
-router.post("/create", uploadFile.single('imageProduct'), productsController.store);
 
 module.exports = router;
 
